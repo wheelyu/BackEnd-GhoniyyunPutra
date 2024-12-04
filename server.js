0b91dfc6a03f56f388b8052baee8d4ab3b9fbdfd
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-05 01:45:38 +0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const sequelize = require('./config/database');
 const categoryRoutes = require('./routes/CategoryRoutes');
 const productRoutes = require('./routes/ProductRoutes');
 const corsMiddleware = require('./middleware/corsMiddleware'); // Impor middleware
+const path = require('path'); // Impor path untuk penanganan file
+require('dotenv').config(); // Tambahkan dotenv untuk membaca file .env
 
 const app = express();
 
@@ -13,16 +15,20 @@ app.use(express.json());
 // Routes
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
-// In your server.js
-app.use('/uploads/products', express.static('uploads/products'));
 
-app.get('/*', (req, res) => res.sendFile(__dirname + '/index.html'));
+// Static files untuk uploads
+app.use('/uploads/products', express.static(path.join(__dirname, 'uploads/products')));
+
+// Serve index.html untuk semua route lain
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
 // Sinkronisasi basis data
 sequelize
   .sync()
   .then(() => console.log('Database synchronized'))
   .catch((error) => console.error('Database synchronization error:', error));
 
+// Menentukan port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di port ${PORT}`);
