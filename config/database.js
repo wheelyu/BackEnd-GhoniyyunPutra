fcc8cd9c400bf55083f8016539647fc75866fd2c
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-09 01:40:04 +0700

    apdet

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('if0_37874893_ghoniyyunputra', 'if0_37874893', '3blW2nxq1DZ3p ', {
-  host: 'sql312.infinityfree.com',
+const sequelize = new Sequelize('ghoniyyunputra_guiderace', 'ghoniyyunputra_guiderace', '5a6a9f6736a7cd78f4afb8b04100e1a96c13bae6', {
+  host: 'yvgup.h.filess.io',
   dialect: 'mysql',
-  port: 3306,
+  port: 3307,
   dialectOptions: {
     ssl: {
       require: true, // biasanya diperlukan untuk koneksi cloud
