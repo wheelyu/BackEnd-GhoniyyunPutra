2bc33c7823afb6703a21b827cdc45e919490253e
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-08 22:12:16 +0700

    update be

--- config/database.js
+++ config/database.js
@@ -1,10 +1,17 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ghoniyyunputra', 'root', null, {
-  host: 'localhost',
+const sequelize = new Sequelize('if0_37874893_ghoniyyunputra', 'if0_37874893', '3blW2nxq1DZ3p ', {
+  host: 'sql312.infinityfree.com',
   dialect: 'mysql',
+  port: 3306,
+  dialectOptions: {
+    ssl: {
+      require: true, // biasanya diperlukan untuk koneksi cloud
+      rejectUnauthorized: false // opsional, tergantung konfigurasi SSL Anda
+    }
+  },
   timezone: '+07:00',
   logging: false
 });
 
-module.exports = sequelize;
+module.exports = sequelize; 
