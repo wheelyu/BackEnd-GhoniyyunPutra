9bca3cac81ac8e1353d70807d3aa021785593d12
wheelyu <harun.abdulkarim17@gmail.com>
2024-12-11 11:38:50 +0700

    psuh

--- config/database.js
+++ config/database.js
@@ -1,15 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ghoniyyunputra_guiderace', 'ghoniyyunputra_guiderace', '5a6a9f6736a7cd78f4afb8b04100e1a96c13bae6', {
-  host: 'yvgup.h.filess.io',
+const sequelize = new Sequelize('ghoniyyunputra', 'root', null, {
+  host: 'localhost',
   dialect: 'mysql',
-  port: 3307,
-  dialectOptions: {
-    ssl: {
-      require: true, // biasanya diperlukan untuk koneksi cloud
-      rejectUnauthorized: false // opsional, tergantung konfigurasi SSL Anda
-    }
-  },
   timezone: '+07:00',
   logging: false
 });
